02186d9ea3fad47ecde4b42645339c3b49bf1290
Francisco Castañeda <fra.castaneda@gmail.com>
2021-05-31 22:28:07 -0400

    aRmbian_V_0.1.2.1

--- Report.sh
+++ Report.sh
@@ -5,6 +5,7 @@ echo [=== Configuration ===]
 rm ~/.xinitrc
 # only one html supported, for multiple html files in folder write full filename of desire report
 echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nchromium-browser /media/Reports/*.html --no-sandbox --kiosk --fullscreen" >> ~/.xinitrc
+chmod a+x  ~/.xinitrc
 echo  [=== Rmarkdown Script ===]
 # only one Rmd supported, for multiple Rmd files in folder write full filename of desire report
 Rscript -e "rmarkdown::render('/media/Reports/*.Rmd')" 
