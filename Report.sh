0af5db6f63aa4ae796997b90429d762faf6a4a21
Francisco Castañeda <fra.castaneda@gmail.com>
2021-06-04 14:04:44 -0400

    aRmbian_V_0.1.3.2

--- Report.sh
+++ Report.sh
@@ -2,13 +2,15 @@
 echo [=== aRmbian - Statistics Reports ===]
 echo
 echo [=== Configuration ===]
+cd /media
+git clone https://github.com/fra-castaneda/COVID-Chile
 touch ~/.xsession
 # Change to report name
-echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nchromium-browser /media/Reports/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
+echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nchromium-browser /media/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
 chmod a+x  ~/.xsession
 echo  [=== Rmarkdown Script ===]
 # Change to report name
-Rscript -e "rmarkdown::render('/media/Reports/COVID-Chile.Rmd')" 
+Rscript -e "rmarkdown::render('/media/COVID-Chile.Rmd')" 
 
 echo  [=== Print Report  ===]
 startx
