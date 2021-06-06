b4484244a85e0cff0215816cc1cdfc4e10e19eb1
Francisco Castañeda <fra.castaneda@gmail.com>
2021-06-06 14:37:26 -0400

    aRmbian_V_0.1.3.5

--- Report.sh
+++ Report.sh
@@ -4,14 +4,12 @@ echo
 echo [=== Configuration ===]
 cp conky.conf /etc/conky/conky.conf
 cd /media
-git clone https://github.com/fra-castaneda/COVID-Chile/COVID-Chile
+git clone https://github.com/fra-castaneda/COVID-Chile
 rm ~/.xsession
 # Change to report name
-echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nxsetroot -solid seagreen \nxclock -d -update -geom 210x30-5+10 & \nconky & \nchromium-browser /media/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
+echo -e "#!/bin/sh \nxset -dpms \nxset s off \nxset s noblank \nxsetroot -solid seagreen \nxclock -d -update 1 -geom 215x30-0+10 & \nconky & \nxterm ~/Update.sh -geom 35x20-0+400 & \nxeyes -geom 215x200-0+670 & \nchromium-browser /media/COVID-Chile/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
 chmod a+x  ~/.xsession
-echo  [=== Rmarkdown Script ===]
-# Change to report name
-Rscript -e "rmarkdown::render('/media/COVIS-Chile/COVID-Chile.Rmd')" 
+rm ~/Update.sh
+echo -e"#!/bin/sh \n[=== Rmarkdown Script ===] \n# Change to report name \nRscript -e "rmarkdown::render('/media/COVID-Chile/COVID-Chile.Rmd')"" >> ~/Update.sh
+chmod +x ~/Update.sh
 
-echo  [=== Print Report  ===]
-startx
