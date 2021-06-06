001fc0e2ef3ea7fbfc4442865d8b3539edb6d538
Francisco Castañeda <fra.castaneda@gmail.com>
2021-06-05 23:24:01 -0400

    aRmbian_V_0.1.3.3

--- Report.sh
+++ Report.sh
@@ -2,15 +2,16 @@
 echo [=== aRmbian - Statistics Reports ===]
 echo
 echo [=== Configuration ===]
+cp conky.conf /etc/conky/conky.conf
 cd /media
-git clone https://github.com/fra-castaneda/COVID-Chile
-touch ~/.xsession
+git clone https://github.com/fra-castaneda/COVID-Chile/COVID-Chile
+rm ~/.xsession
 # Change to report name
-echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nchromium-browser /media/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
+echo -e "#!/bin/sh \nxset -dpms  \nxset s off \nxset s noblank \nxsetroot -solid seagreen \nxclock -d -update -geom 210x30-5+10 & \nconky & \nchromium-browser /media/COVID-Chile.html --no-sandbox --kiosk --fullscreen" >> ~/.xsession
 chmod a+x  ~/.xsession
 echo  [=== Rmarkdown Script ===]
 # Change to report name
-Rscript -e "rmarkdown::render('/media/COVID-Chile.Rmd')" 
+Rscript -e "rmarkdown::render('/media/COVIS-Chile/COVID-Chile.Rmd')" 
 
 echo  [=== Print Report  ===]
 startx
--- aRmbian.sh
+++ aRmbian.sh
@@ -13,7 +13,7 @@ echo [=== aRmbian  - Depencies ===]
 echo
 apt update
 apt upgrade -y
-apt install -y build-essential libssl-dev libgit2-dev libcurl4-openssl-dev libpoppler-cpp-dev libxml2-dev libudunits2-dev libcairo2-dev libgdal-dev texlive pandoc  r-recommended r-base-core r-base r-base-dev xorg chromium-browser neofetch
+apt install -y build-essential libssl-dev libgit2-dev libcurl4-openssl-dev libpoppler-cpp-dev libxml2-dev libudunits2-dev libcairo2-dev libgdal-dev texlive pandoc  r-recommended r-base-core r-base r-base-dev xorg chromium-browser conky
 sudo su - -c "R -e \"install.packages('devtools')\""
 sudo su - -c "R -e \"install.packages('multcomp')\""
 sudo su - -c "R -e \"install.packages('tidyverse')\""
